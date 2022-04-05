9523cbd26449591597ab3d642685f8a7532cddbf
Nicholas Garcia <87832673+nagarci7@users.noreply.github.com>
2022-04-05 16:28:51 -0500

    updates to basic code

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
 /**
- * Using a callback function.
- * @link https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
+ * Passing data to functions through parameters.
+ * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+ *
+ * List of ISO language codes:
+ * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-// Helper function to format currency numbers. Used by tipCalculator
-const formatter = (locale = "en-US", currency = "USD", value) => {
-  let formattedValue = new Intl.NumberFormat(locale, {
-    style: "currency",
-    currency: currency,
-  }).format(value);
-
-  return formattedValue;
-};
-
-// Callback receives finalTip object, creates and outputs table on the DOM.
-const printHTML = (finalTip) => {
-  const tipTable = document.createElement("table");
-  tipTable.innerHTML = `
-    <tr>
-      <td>Sum before tip:</td>
-      <td>${finalTip.sum}</td>
-    </tr>
-    <tr>
-      <td>Tip percentage:</td>
-      <td>${finalTip.percentage}</td>
-    </tr>
-    <tr>
-      <td>Tip:</td>
-      <td>${finalTip.tip}</td>
-    </tr>
-    <tr>
-      <td>Total:</td>
-      <td>${finalTip.total}</td>
-    </tr>
-  `;
-  document.querySelector("main").append(tipTable);
-};
-
-// Create a finalTip object with all the data. Send it to the printHTML callback.
-const tipCalculator = (sum, percentage, locale, currency) => {
-  let tip = sum * (percentage / 100);
-  let total = sum + tip;
-
-  const finalTip = {
-    sum: formatter(locale, currency, sum),
-    percentage: percentage + "%",
-    tip: formatter(locale, currency, tip),
-    total: formatter(locale, currency, total),
+ const formatter = (locale = "en-US", currency = "USD", value) => {
+    let formattedValue = new Intl.NumberFormat(locale, {
+      style: "currency",
+      currency: currency,
+    }).format(value);
+  
+    return formattedValue;
   };
-};
-
-tipCalculator(29.95, 18, "de-DE", "EUR");
+  
+  const tipCalculator = (sum, percentage, locale, currency) => {
+    let tip = sum * (percentage / 100);
+    let total = sum + tip;
+  
+    console.log(`
+      Sum before tip: ${formatter(locale, currency, sum)}
+      Tip percentage: ${percentage}%
+      Tip:            ${formatter(locale, currency, tip)}
+      Total:          ${formatter(locale, currency, total)}
+    `);
+  };
+  
+  tipCalculator(29.95, 18, "en", "USD");
+  
